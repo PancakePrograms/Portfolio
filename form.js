04b1e88ad34d73f00cd890a251c6f973a91eec86
PancakePrograms <charliemaerose8@gmail.com>
2024-09-21 13:07:18 -0400

    update

--- form.js
+++ form.js
@@ -1,3 +1,6 @@
+//Change everything into a tabel
+
+
 const form = document.getElementById('form');
 const first = form.elements['first'];
 const last = form.elements['last'];
@@ -7,9 +10,12 @@ function checkResult(event) {
     event.preventDefault();
     const firstResult = first.value;
     const lastResult = last.value;
-    //for some reason this result.value is only showing first
-    //for later, fix the + lastResult;
-    result.value = firstResult +  lastResult;
+    result.value += firstResult + " " + lastResult + lineBreak;
+
+form.addEventListener('submit', checkResult);;
+    
+    first.value = ""
+    last.value = ""
 }
 
 form.addEventListener('submit', checkResult);
