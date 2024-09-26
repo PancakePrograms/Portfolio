f7ae81f808c018b65a77a0d5dba175f125030aa1
PancakePrograms <charliemaerose8@gmail.com>
2024-09-26 04:11:47 -0400

    update

--- form.js
+++ form.js
@@ -10,7 +10,7 @@ function check() {
     const lastResult = last.value;
     const numResult = num.value;
     resultsDiv.innerHTML += '<tr>' + '<td>' + firstResult + '</td>' + '<td>' + lastResult + '</td>' + '<td>' + numResult + '</td>' + '</tr>'
-    //add a remove entry button. (floating outside the table but inline with the entry it will remove if clicked)
+    //add a remove entry button inside one of the cells, then use css positioning to place that button elsewhere
     first.value = "";
     last.value = "";
     num.value = "";
