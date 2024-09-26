f564049103133556b217b076c7733acf88aca6cc
charlie <charliemaerose8@gmail.com>
2024-09-26 03:57:00 -0400

    update

--- form.js
+++ form.js
@@ -1,15 +1,23 @@
 const form = document.getElementById('form');
 const first = form.elements['first'];
 const last = form.elements['last'];
+const num = form.elements['num'];
 const resultsDiv = document.getElementById('result');
+
 
 function check() {
     const firstResult = first.value; 
     const lastResult = last.value;
-    resultsDiv.innerHTML += '<tr>' + '<td>' + firstResult + '</td>' + '<td>' + lastResult + '</td>' + '</tr>';
-// Add Age. Turn form into a popup window. Add a delete option for new objects.
+    const numResult = num.value;
+    resultsDiv.innerHTML += '<tr>' + '<td>' + firstResult + '</td>' + '<td>' + lastResult + '</td>' + '<td>' + numResult + '</td>' + '</tr>'
+    //add a remove entry button. (floating outside the table but inline with the entry it will remove if clicked)
     first.value = "";
     last.value = "";
+    num.value = "";
 }
 
 form.addEventListener('submit', checkResult);
+function deleteRow(btn) {
+    const row = btn.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
