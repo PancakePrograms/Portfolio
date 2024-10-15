9070e3f73fc6fb6281ad5abde311f06249c4eaa1
charlie <charliemaerose8@gmail.com>
2024-10-15 10:09:59 -0400

    update

--- Upal.js
+++ Upal.js
@@ -1,113 +1,69 @@
-let happiness = 100
-let hunger = 75
-let bathroom = 30
-let cleanliness = 60
-const stats = document.getElementById('stats')
-let life = 1
+let happiness = 100;
+let hunger = 75;
+let bathroom = 30;
+let cleanliness = 60;
+const stats = document.getElementById('stats');
+let life = 1;
 
-function food() {
+const updateStats = () => {
+  stats.innerHTML = `
+    Happiness: ${happiness}<br>
+    Hunger: ${hunger}<br>
+    Bathroom: ${bathroom}<br>
+    Cleanliness: ${cleanliness}
+  `;
+};
+
+const food = () => {
   hunger += 30;
-  
-  if (hunger > 100) {
-    hunger = 100;
-  }
-  
-  if(bathroom < 30) {
+  hunger = Math.min(hunger, 100);
+
+  if (bathroom < 30) {
     return;
   }
-  if (bathroom === 30) {
-    bathroom -= 1
-  }
-  let poop = setInterval(() => {
-    
+
+  bathroom -= 1;
+  const poopInterval = setInterval(() => {
     bathroom -= 1;
-    
-    
-    if(bathroom < 1) {
-      bathroom +=30;
-    clearInterval(poop)
+
+    if (bathroom < 1) {
+      bathroom += 30;
+      clearInterval(poopInterval);
     }
-    
-  stats.innerHTML = 
-  "Happiness: " + happiness + '<br>' +
-"Hunger: " + hunger + '<br>' +
-    "Bathroom: " + bathroom + '<br>' +
-"Cleanliness: " + cleanliness; 
-     
-  }, 1000)
-  
-  stats.innerHTML = 
-  "Happiness: " + happiness + '<br>' +
-"Hunger: " + hunger + '<br>' +
-    "Bathroom: " + bathroom + '<br>' +
-"Cleanliness: " + cleanliness;
-}
 
-function clean() {
-  clean += 75
-  
-  if (cleanliness > 100) {
-    cleanliness = 100
-    
-    stats.innerHTML = 
-  "Happiness: " + happiness + '<br>' +
-"Hunger: " + hunger + '<br>' +
-    "Bathroom: " + bathroom + '<br>' +
-"Cleanliness: " + cleanliness;
-  }
-}
+    updateStats();
+  }, 1000);
 
-if (happiness > 100) {
-    happiness = 100;
-}
-if (bathroom > 100) {
-    bathroom = 100;
-}
-if (cleanliness > 100) {
-    cleanliness = 100;
-}
-stats.innerHTML = 
-"Happiness: " + happiness + '<br>' +
-"Hunger: " + hunger + '<br>' +
-    "Bathroom: " + bathroom + '<br>' +
-"Cleanliness: " + cleanliness;
+  updateStats();
+};
 
+const clean = () => {
+  cleanliness += 75;
+  cleanliness = Math.min(cleanliness, 100);
+  updateStats();
+};
 
 const fullDay = 24 * 60 * 60 * 1000;
 const halfDay = 1 * 60 * 60 * 1000;
 
-setInterval(function() {
-    stats.innerHTML = 
-"Happiness: " + happiness + '<br>' +
-"Hunger: " + hunger + '<br>' +
-    "Bathroom: " + bathroom + '<br>' +
-"Cleanliness: " + cleanliness;
+setInterval(() => {
+  hunger -= 8;
+  cleanliness -= 4;
 
-
-hunger -= 8;
-cleanliness -= 4;
-
-if (hunger < 0) {
+  if (hunger < 0 || cleanliness < 0) {
     life = 0;
-}
+  }
 
-if (cleanliness < 0) {
-    life = 0;
-
-}
-if (life === 0) {
-    //this will do something more dramatic later
+  if (life === 0) {
     stats.innerHTML = 'dead';
-}
+  }
 
+  updateStats();
 }, halfDay);
 
-setInterval(function() {
-    stats.innerHTML = 
-"Happiness: " + happiness + '<br>' +
-"Hunger: " + hunger + '<br>' +
-    "Bathroom: " + bathroom + '<br>' +
-"Cleanliness: " + cleanliness;
-
-happiness -= 12;
+setInterval(() => {
+  happiness -= 12;
+  updateStats();
 }, halfDay);
+
+updateStats();
