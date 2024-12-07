23eb2b0c3146be8e252d8d6c42d9abdcd6d7aa58
PancakePrograms <charliemaerose8@gmail.com>
2024-12-07 03:48:45 -0500

    update

--- template.js
+++ template.js
@@ -7,14 +7,24 @@ const x = 'false';
 const rewards = document.querySelector('.reward');
 const content = document.querySelector('.content');
 const home = document.querySelector('.Home');
+const rewards_page = document.querySelector('.rewards_section')
 
 rewards.addEventListener('click', () => {
     if(content.style.display === 'flex' || content.style.display === '') {
         content.style.display = 'none';
         home.style.color = 'black';
         rewards.style.color = 'white';
+        rewards_page.style.display = 'flex';
     };
 });
+home.addEventListener('click', () => {
+  if (content.style.display === 'none') {
+  content.style.display = 'flex';
+  rewards_page.style.display = 'none';
+  rewards.style.color = 'black';
+  home.style.color = 'white';
+  }
+})
 
 function open_sideBar() {
     topBar.style.transform = 'translateY(8px) rotate(45deg)';
@@ -38,25 +48,36 @@ burger.addEventListener('click', () => {
     }
 })
 
-const login_button = document.querySelector('.top-bar :nth-child(1)');
-const login = document.querySelector('.login');
-
-function open_login() {
-    login.style.visibility = 'visible';
+let pointVal = 100; 
+const redeem = document.querySelector('.rewards_section button')
+let minPoint = 0
+function updatePoints() {
+  document.getElementById('point_root').innerHTML = pointVal + " Points";
+  console.log("points")
+  if(pointVal >= 200) {
+    console.log("redeem available")
+    redeem.style.backgroundColor = "orange";
+  } else {
+    redeem.style.backgroundColor = '#9F9D9A';
+  }
+  if (pointVal < minPoint) {
+    pointVal = minPoint;
+  }
 }
-function close_login() {
-    login.style.visibility = 'hidden';
-}
+updatePoints();
 
-login_button.addEventListener('click', () => {
-
-    if (login.style.visibility === 'hidden' || login.style.visibility === '') {
-        open_login();
-    }
+document.getElementById('increase').addEventListener('click', () => {
+  pointVal += 25;
+  updatePoints();
 });
 
-const close_button =document.querySelector('.close');
-
-close_button.addEventListener('click', () => {
-    close_login();
+document.getElementById('decrease').addEventListener('click', () => {
+  if(pointVal <= 0) {
+    pointVal = 0;
+    updatePoints();
+    return
+  } else {
+  pointVal -= 25;
+  updatePoints();
+  }
 });
