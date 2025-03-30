da4eadcb8e49be7359b55bba1a6e1952a8d3472b
charlie <charliemaerose8@gmail.com>
2025-03-30 05:04:23 -0400

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+const home = document.querySelector('.home_body');
+const certTab = document.querySelector('.certification_div')
+function cert() {
+    home.style.display = 'none';
+    certTab.style.display = 'block';
+}
+function test() {
+    console.log('test')
+}
+test()
+console.log("hello world")
