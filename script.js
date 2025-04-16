fd171a7383487d67d1479e10e66ca4973f7e1565
charlie <charliemaerose8@gmail.com>
2025-04-16 09:19:41 -0400

    overlow issues need work

--- script.js
+++ script.js
@@ -9,5 +9,19 @@ burger.addEventListener('click', () => {
         burgerMenu.style.right = '-100vw';
         console.log('closed')
     }
-    
+});
+
+const Certifications = document.querySelector(".Certifications");
+const intro = document.querySelector('.intro');
+const certTab = document.querySelector('.certTab')
+
+certTab.addEventListener('click', () => {
+        intro.style.display = 'none';
+        Certifications.style.display = 'flex';
+})
+const homeTab = document.querySelector('.homeTab');
+
+homeTab.addEventListener('click', () => {
+    intro.style.display = 'block';
+    Certifications.style.display = 'none';
 })
