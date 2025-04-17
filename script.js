3e94ca0422dae5047711978c792ff2d8690a0b08
charlie <charliemaerose8@gmail.com>
2025-04-17 09:46:20 -0400

    cert page complete

--- script.js
+++ script.js
@@ -18,10 +18,56 @@ const certTab = document.querySelector('.certTab')
 certTab.addEventListener('click', () => {
         intro.style.display = 'none';
         Certifications.style.display = 'flex';
+        certTab.style.color = 'rgb(145, 202, 250)'
+        homeTab.style.color = 'white'
 })
 const homeTab = document.querySelector('.homeTab');
 
 homeTab.addEventListener('click', () => {
     intro.style.display = 'block';
     Certifications.style.display = 'none';
+    homeTab.style.color = 'rgb(145, 202, 250)';
+    certTab.style.color = 'white'
+})
+
+const html = document.querySelector('.html');
+const htmlCert = document.querySelector('.htmlCert')
+
+html.addEventListener('mouseover', () => {
+    console.log('hovered')
+    htmlCert.style.visibility = 'visible';
+    htmlCert.style.width = '80vw';
+    html.style.color = 'rgb(145, 202, 250)';
+})
+html.addEventListener('mouseout', () => {
+    htmlCert.style.visibility = 'hidden';
+    htmlCert.style.width = '0vw'
+    html.style.color = 'white'
+})
+const css = document.querySelector('.css');
+const cssCert = document.querySelector('.cssCert');
+
+css.addEventListener('mouseover', () => {
+    cssCert.style.visibility = 'visible';
+    cssCert.style.width = '80vw';
+    css.style.color = 'rgb(145, 202, 250)';
+})
+css.addEventListener('mouseout', () => {
+    cssCert.style.visibility = 'visible';
+    cssCert.style.width = '0'
+    css.style.color = 'white';
+})
+
+const js = document.querySelector('.JS');
+const jsCert = document.querySelector('.jsCert');
+
+js.addEventListener('mouseover', () => {
+    jsCert.style.visibility = 'visible';
+    jsCert.style.width = '80vw';
+    js.style.color = 'rgb(145, 202, 250)';
+})
+js.addEventListener('mouseout', () => {
+    jsCert.style.visibility = 'hidden';
+    jsCert.style.width = '0'
+    js.style.color = 'white';
 })
